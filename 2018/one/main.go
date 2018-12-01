7f9e10edf4419a81c028269a86aa16f840df415e
Michael Krysiak <mkrysiak@gmail.com>
2018-12-01 14:37:56 -0500

    Refactored and added tests

--- 2018/one/main.go
+++ 2018/one/main.go
@@ -3,44 +3,104 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"strconv"
-	"strings"
 )
 
-func main() {
-	var freq, loop int
-	dup := make(map[int]struct{})
+// func main() {
+// 	freq := 0
+// 	loop := false
+// 	dup := map[int]struct{}{}
 
-	file, _ := os.Open("input")
-	defer file.Close()
-	reader := bufio.NewReader(file)
+// 	file, _ := os.Open("input")
+// 	defer file.Close()
+// 	reader := bufio.NewReader(file)
+
+// 	for {
+// 		str, err := reader.ReadString('\n')
+// 		if err == io.EOF {
+// 			if !loop {
+// 				fmt.Printf("Part 1: %d\n", freq)
+// 			}
+// 			loop = true
+// 			file.Seek(0, 0)
+// 			continue
+// 		}
+
+// 		str = strings.TrimSpace(str)
+// 		val, err := strconv.Atoi(str)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+
+// 		freq += val
+
+// 		if _, ok := dup[freq]; ok {
+// 			fmt.Printf("Part 2: %d\n", freq)
+// 			break
+// 		}
+
+// 		dup[freq] = struct{}{}
+// 	}
+// }
+
+func main() {
+	contents := readInputFile(os.Args[1])
+	frequency := part1(contents)
+	duplicate := part2(contents)
+	fmt.Printf("Part 1: %d\nPart 2: %d\n", frequency, duplicate)
+}
+
+func part1(contents *[]string) int {
+	freq := 0
+	for _, line := range *contents {
+
+		val, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to convert string to int: %s\n", err)
+		}
+		freq += val
+	}
+	return freq
+
+}
+
+func part2(contents *[]string) int {
+
+	freq := 0
+	dup := map[int]struct{}{}
+
+	dup[0] = struct{}{}
 
 	for {
-		str, err := reader.ReadString('\n')
-		if err == io.EOF {
-			if loop == 0 {
-				fmt.Printf("Part 1: %d\n", freq)
+		for _, line := range *contents {
+
+			val, err := strconv.Atoi(line)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Unable to convert string to int: %s\n", err)
 			}
-			loop++
-			file.Seek(0, 0)
-			continue
-		}
 
-		str = strings.TrimSpace(str)
-		val, err := strconv.Atoi(str)
-		if err != nil {
-			fmt.Println(err)
-		}
+			freq += val
 
-		freq += val
-
-		if _, ok := dup[freq]; ok {
-			fmt.Printf("Part 2: %d\n", freq)
-			break
-		} else {
+			if _, ok := dup[freq]; ok {
+				return freq
+			}
 			dup[freq] = struct{}{}
 		}
 	}
+}
+
+func readInputFile(infile string) *[]string {
+
+	file, err := os.Open(infile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to open file: %s\n", err)
+	}
+	var contents []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		contents = append(contents, scanner.Text())
+	}
+
+	return &contents
 }
