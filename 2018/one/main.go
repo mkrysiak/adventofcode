ac1649e2e5460f06603bb755f332629f70367542
Michael Krysiak <mkrysiak@gmail.com>
2018-12-01 14:39:10 -0500

    Refactored and added tests

--- 2018/one/main.go
+++ 2018/one/main.go
@@ -7,43 +7,6 @@ import (
 	"strconv"
 )
 
-// func main() {
-// 	freq := 0
-// 	loop := false
-// 	dup := map[int]struct{}{}
-
-// 	file, _ := os.Open("input")
-// 	defer file.Close()
-// 	reader := bufio.NewReader(file)
-
-// 	for {
-// 		str, err := reader.ReadString('\n')
-// 		if err == io.EOF {
-// 			if !loop {
-// 				fmt.Printf("Part 1: %d\n", freq)
-// 			}
-// 			loop = true
-// 			file.Seek(0, 0)
-// 			continue
-// 		}
-
-// 		str = strings.TrimSpace(str)
-// 		val, err := strconv.Atoi(str)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-
-// 		freq += val
-
-// 		if _, ok := dup[freq]; ok {
-// 			fmt.Printf("Part 2: %d\n", freq)
-// 			break
-// 		}
-
-// 		dup[freq] = struct{}{}
-// 	}
-// }
-
 func main() {
 	contents := readInputFile(os.Args[1])
 	frequency := part1(contents)
