beea41aecac1c57e38c8522f98a774ec83c4a97f
Michael Krysiak <mkrysiak@gmail.com>
2018-12-02 08:53:36 -0500

    Day 2 solution

---
+++ 2018/2/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	contents := readInputFile(os.Args[1])
+	checksum := part1(contents)
+	fmt.Printf("Part 1: %d\n", checksum)
+	fmt.Printf("Part 2: %s\n", part2(contents))
+}
+
+func part1(contents *[]string) int {
+	var countTwos, countThrees int
+	for _, v := range *contents {
+		twos, threes := hasTwosOrThrees(v)
+		if twos {
+			countTwos++
+		}
+		if threes {
+			countThrees++
+		}
+	}
+	return countTwos * countThrees
+}
+
+func hasTwosOrThrees(s string) (bool, bool) {
+	var twos, threes bool
+	charCounter := map[rune]int{}
+	for _, c := range s {
+		if _, ok := charCounter[c]; ok {
+			charCounter[c]++
+		} else {
+			charCounter[c] = 1
+		}
+	}
+	for _, c := range charCounter {
+		if c == 2 {
+			twos = true
+		} else if c == 3 {
+			threes = true
+		}
+	}
+	return twos, threes
+}
+
+func part2(contents *[]string) string {
+	for _, s := range *contents {
+		for _, t := range *contents {
+			index := charMismatchIndex(s, t)
+			if index >= 0 {
+				// Make a copy of the string, less the mismatching character
+				currentRune := []rune(s)
+				newRune := make([]rune, len(currentRune)-1)
+				j := 0
+				for i, v := range currentRune {
+					if i == index {
+						continue
+					}
+					newRune[j] = v
+					j++
+				}
+				return string(newRune)
+			}
+		}
+	}
+	return ""
+}
+
+func charMismatchIndex(s1, s2 string) int {
+	r1 := []rune(s1)
+	r2 := []rune(s2)
+	var diff, index int
+	for i := 0; i < len(s1); i++ {
+		if r1[i] != r2[i] {
+			diff++
+			index = i
+		}
+		// Return early if there is more than one character mismatch
+		if diff > 1 {
+			return -1
+		}
+	}
+	// Return false if there is an exact match
+	if diff == 0 {
+		return -1
+	}
+	return index
+}
+
+func readInputFile(infile string) *[]string {
+
+	file, err := os.Open(infile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to open file: %s\n", err)
+	}
+	var contents []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		contents = append(contents, scanner.Text())
+	}
+
+	return &contents
+}
---
+++ 2018/2/main_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var part1Tests = []struct {
+	in     string
+	twos   bool
+	threes bool
+}{
+	{"abcdef", false, false},
+	{"bababc", true, true},
+	{"abbcde", true, false},
+	{"abcccd", false, true},
+	{"aabcdd", true, false},
+	{"abcdee", true, false},
+	{"ababab", false, true},
+}
+
+func TestPart1HasTwoOrThree(t *testing.T) {
+	for _, tt := range part1Tests {
+		t.Run("Part 1 - hasTwosOrThrees", func(t *testing.T) {
+			twos, threes := hasTwosOrThrees(tt.in)
+			assert.Equal(t, tt.twos, twos)
+			assert.Equal(t, tt.threes, threes)
+		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	part1Test := []string{
+		"abcdef",
+		"bababc",
+		"abbcde",
+		"abcccd",
+		"aabcdd",
+		"abcdee",
+		"ababab",
+	}
+
+	t.Run("Part 1", func(t *testing.T) {
+		checksum := part1(&part1Test)
+		assert.Equal(t, 12, checksum)
+	})
+}
+
+func TestPart2(t *testing.T) {
+	part2Test := []string{
+		"abcde",
+		"fghij",
+		"klmno",
+		"pqrst",
+		"fguij",
+		"axcye",
+		"wvxyz",
+	}
+
+	t.Run("Part 2", func(t *testing.T) {
+		match := part2(&part2Test)
+		assert.Equal(t, "fgij", match)
+	})
+}
