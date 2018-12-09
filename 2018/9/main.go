9510890ed545c649a7f893016b9130e452f23d46
Michael Krysiak <mkrysiak@gmail.com>
2018-12-09 07:25:21 -0500

    2018 Day 9

---
+++ 2018/9/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	playerCount, _ := strconv.Atoi(os.Args[1])
+	lastMarble, _ := strconv.Atoi(os.Args[2])
+	fmt.Printf("Part 1: %d\n", part1(playerCount, lastMarble))
+}
+
+func part1(playerCount int, lastMarble int) int {
+	var players = make([]int, playerCount)
+	r := ring.New(1)
+	r.Value = 0
+	// head := r
+
+	currPlayer := 0
+	for turn := 1; turn <= lastMarble; turn++ {
+		// head.Do(func(p interface{}) {
+		// 	fmt.Printf("%d ", p.(int))
+		// })
+		// fmt.Println()
+
+		if (turn % 23) == 0 {
+			players[currPlayer] += turn
+			players[currPlayer] += r.Move(-8).Value.(int)
+
+			// ring.Unlink() removes r.Next(), not r.  So move back one more.
+			r = r.Move(-9)
+			r.Unlink(1)
+			r = r.Next().Next()
+		} else {
+			newMarble := ring.New(1)
+			newMarble.Value = turn
+
+			r.Link(newMarble)
+			r = r.Next().Next()
+		}
+		currPlayer++
+		if currPlayer > (playerCount - 1) {
+			currPlayer = 0
+		}
+	}
+
+	highestScore := 0
+	for _, v := range players {
+		if v > highestScore {
+			highestScore = v
+		}
+	}
+	return highestScore
+}
