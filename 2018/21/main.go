cc37481c4f694bf04ce539e3f50c5888eca4b101
Michael Krysiak <mkrysiak@gmail.com>
2018-12-21 10:52:31 -0500

    2018 Day 21

--- 2018/21/main.go
+++ 2018/21/main.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 )
 
-var registers = [6]int{}
 var opCodes = map[string]interface{}{
 	"addr": addr,
 	"addi": addi,
@@ -27,16 +26,15 @@ var opCodes = map[string]interface{}{
 }
 
 func main() {
-	fmt.Printf("Part 1: %d\n", part1(input()))
-	registers = [6]int{1, 0, 0, 0, 0, 0}
-	fmt.Printf("Part 2: %d\n", part2(input()))
+	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
 }
 
-func part1(in string) int {
-	lines := strings.Split(in, "\n")
+func part1() int {
+	registers := [6]int{0, 0, 0, 0, 0, 0}
+	lines := strings.Split(input(), "\n")
 	instrReg, _ := strconv.Atoi(strings.Fields(lines[0])[1])
 	lines = lines[1:]
-
 	for registers[instrReg] < len(lines) {
 		f := strings.Fields(string(lines[registers[instrReg]]))
 		op := f[0]
@@ -44,20 +42,24 @@ func part1(in string) int {
 		in2, _ := strconv.Atoi(f[2])
 		out, _ := strconv.Atoi(f[3])
 
+		if op == "eqrr" {
+			return registers[4]
+		}
 		// fmt.Printf("ip=%d [%d, %d, %d, %d, %d, %d] ", registers[instrReg], registers[0], registers[1], registers[2], registers[3], registers[4], registers[5])
 		opCodes[op].(func([4]int, *[6]int))([4]int{0, in1, in2, out}, &registers)
 		// fmt.Printf("%s %d %d %d [%d, %d, %d, %d, %d, %d]\n", op, in1, in2, out, registers[0], registers[1], registers[2], registers[3], registers[4], registers[5])
 		registers[instrReg]++
 	}
-	return registers[0]
+	return 0
 }
 
-func part2(in string) int {
-	lines := strings.Split(in, "\n")
+func part2() int {
+	registers := [6]int{0, 0, 0, 0, 0, 0}
+	lines := strings.Split(input(), "\n")
 	instrReg, _ := strconv.Atoi(strings.Fields(lines[0])[1])
 	lines = lines[1:]
-
-	accumulator := 0
+	seen := map[int]struct{}{}
+	prev := 0
 	for registers[instrReg] < len(lines) {
 		f := strings.Fields(string(lines[registers[instrReg]]))
 		op := f[0]
@@ -65,24 +67,24 @@ func part2(in string) int {
 		in2, _ := strconv.Atoi(f[2])
 		out, _ := strconv.Atoi(f[3])
 
+		if op == "eqrr" {
+			if _, ok := seen[registers[4]]; !ok {
+				seen[registers[4]] = struct{}{}
+				prev = registers[4]
+			} else {
+				return prev
+			}
+		}
 		// fmt.Printf("ip=%d [%d, %d, %d, %d, %d, %d] ", registers[instrReg], registers[0], registers[1], registers[2], registers[3], registers[4], registers[5])
 		opCodes[op].(func([4]int, *[6]int))([4]int{0, in1, in2, out}, &registers)
-		// fmt.Printf("%s %d%d %d [%d, %d, %d, %d, %d, %d]\n", op, in1, in2, out, registers[0], registers[1], registers[2], registers[3], registers[4], registers[5])
-
-		// Study the program and output, to identify the pattern.  See the "output" file.
-		// Run through the first few operations to initalize register[4], and once the
-		// program-counter == 1, register[4] has been initalized.
-		if registers[instrReg] == 1 {
-			for x := 1; x <= registers[4]; x++ {
-				if registers[4]%x == 0 {
-					accumulator += x
-				}
-			}
-			break
-		}
+		// fmt.Printf("%s %d %d %d [%d, %d, %d, %d, %d, %d]\n", op, in1, in2, out, registers[0], registers[1], registers[2], registers[3], registers[4], registers[5])
 		registers[instrReg]++
 	}
-	return accumulator
+	return 0
+}
+
+func execute() {
+
 }
 
 func addr(i [4]int, r *[6]int) {
@@ -173,53 +175,37 @@ func eqrr(i [4]int, r *[6]int) {
 	(*r)[i[3]] = 0
 }
 
-func testInput() string {
-	return `#ip 0
-seti 5 0 1
-seti 6 0 2
-addi 0 1 0
-addr 1 2 3
-setr 1 0 0
-seti 8 0 4
-seti 9 0 5`
-}
-
 func input() string {
 	return `#ip 1
-addi 1 16 1
-seti 1 1 3
-seti 1 9 5
-mulr 3 5 2
-eqrr 2 4 2
+seti 123 0 4
+bani 4 456 4
+eqri 4 72 4
+addr 4 1 1
+seti 0 0 1
+seti 0 2 4
+bori 4 65536 3
+seti 10552971 1 4
+bani 3 255 5
+addr 4 5 4
+bani 4 16777215 4
+muli 4 65899 4
+bani 4 16777215 4
+gtir 256 3 5
+addr 5 1 1
+addi 1 1 1
+seti 27 7 1
+seti 0 1 5
+addi 5 1 2
+muli 2 256 2
+gtrr 2 3 2
 addr 2 1 1
 addi 1 1 1
-addr 3 0 0
+seti 25 0 1
 addi 5 1 5
-gtrr 5 4 2
-addr 1 2 1
-seti 2 6 1
-addi 3 1 3
-gtrr 3 4 2
-addr 2 1 1
-seti 1 6 1
-mulr 1 1 1
-addi 4 2 4
-mulr 4 4 4
-mulr 1 4 4
-muli 4 11 4
-addi 2 6 2
-mulr 2 1 2
-addi 2 2 2
-addr 4 2 4
-addr 1 0 1
-seti 0 3 1
-setr 1 4 2
-mulr 2 1 2
-addr 1 2 2
-mulr 1 2 2
-muli 2 14 2
-mulr 2 1 2
-addr 4 2 4
-seti 0 0 0
-seti 0 4 1`
+seti 17 2 1
+setr 5 7 3
+seti 7 8 1
+eqrr 4 0 5
+addr 5 1 1
+seti 5 0 1`
 }
